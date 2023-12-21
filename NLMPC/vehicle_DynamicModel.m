e124fbf03071b382df69724e95264becde98fdae
WangZiNi <109276096+WangZiNi@users.noreply.github.com>
2023-12-21 13:19:07 +0800

    Update vehicle_DynamicModel.m
    annotation changes

--- NLMPC/vehicle_DynamicModel.m
+++ NLMPC/vehicle_DynamicModel.m
@@ -1,9 +1,8 @@
 function power = vehicle_DynamicModel(Velocity)
-%车辆动力学模型
-%将车辆速度转变成车辆功率需求
-%总共 T s的速度轨迹，转换成共（T-1）s的功率需求
+%vehicle dynamic model
+%transfer vehicle velocity to power demand
 N=length(Velocity);
-%%%%电动汽车模型参数
+%parameters
 elta_md=0.85;
 elta_T=0.9;
 elta_r=0.65;
@@ -12,7 +11,7 @@ g=9.8;
 Cd=0.28;
 Af=2.2;
 p=1.29;
-%%滚动阻力系数
+%%rolling resistance coefficient
 f=0.016;
 calP=m*g*f*Velocity(1:N-1)+0.5*Cd*Af*p*(Velocity(1:N-1)).^3+m*Velocity(1:N-1).*(Velocity(2:N)-Velocity(1:N-1));
 power=calP/(elta_md*elta_T);
